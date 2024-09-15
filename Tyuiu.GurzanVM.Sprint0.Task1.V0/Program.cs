5cf31e4683d76ddc211f512df242a803779dad45
Влад Гурзан <vitalo2233@mail.ru>
2024-09-15 14:49:06 +0500

    Task1

---
+++ Tyuiu.GurzanVM.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GurzanVM.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            string name = "Vlad";
+            int age = 18;
+            Console.WriteLine("Привет меня зовут " + name + " мне " + age + " лет ");
+            Console.ReadLine();
+           
+        }
+    }
+}
