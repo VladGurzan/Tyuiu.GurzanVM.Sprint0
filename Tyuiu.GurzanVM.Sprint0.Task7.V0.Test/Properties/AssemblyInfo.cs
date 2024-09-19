8505c4be35de3339fea1f9c103bf278b2bf96551
Влад Гурзан <vitalo2233@mail.ru>
2024-09-19 17:17:56 +0500

    Task 7

---
+++ Tyuiu.GurzanVM.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GurzanVM.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GurzanVM.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c166ffff-be17-42d2-92f4-d8a6fffc4aed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GurzanVM.Sprint0.Task7.V0/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GurzanVM.Sprint0.Task7.V0.Lib;
+
+
+namespace Tyuiu.GurzanVM.Sprint0.Task7.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #0 | Выполнил: Гурзан В. М. | Смартб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" Спринт #0                                                                 ");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Гурзан Владислав Михайлович | Смартб-24-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать консольную программу на С#, которая суммирует значение двух    *");
+            Console.WriteLine("* одинаковых массивов по длинне.                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Значение элементов массива №1");
+            for (int i = 0;  i < arrayNums1.Length; i++)
+            {
+                Console.Write(arrayNums1[i] + ", ");
+            }
+            Console.WriteLine();
+
+            int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Значение элементов массива №2");
+            for (int i = 0;  i < arrayNums2.Length; i++)
+            {
+                Console.Write(arrayNums2[i] + ", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (arrayNums1.Length == arrayNums2.Length)
+            {     
+                    int[] resultArray = DataServies.AdditionArrays(arrayNums1, arrayNums2);
+
+                    Console.WriteLine("Сумма элементов массива ровна:");
+                for (int i = 0;  i < resultArray.Length; i++)
+                {
+                        Console.Write(resultArray[i] + ", ");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Исходные массива имеют разноет количество элементов");
+            }
+            Console.ReadKey();
+        }    
+    }
+}
